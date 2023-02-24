43b1acb44f35c165ef7869fbe65f316fce7c2fd5
yurtsevenist <yurtsevenist@hotmail.com>
2023-02-24 14:55:24 +0300

    veritabani islemleri 2

--- database/migrations/2023_02_24_113918_create_products_table.php
+++ database/migrations/2023_02_24_113918_create_products_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('who')->default(0)->comment('0 müşteri - 1 yönetici');
-            $table->rememberToken();
+            $table->string('category')->nullable;
+            $table->string('size')->nullable();
+            $table->string('color')->nullable();
+            $table->double('price')->default(0);
+            $table->integer('number')->default(0);
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
