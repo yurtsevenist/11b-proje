6819fbb07069f9f6681be70654a2f1d9a81ad88e
yurtsevenist <yurtsevenist@hotmail.com>
2023-04-07 09:04:35 +0300

    cart 1

---
+++ database/migrations/2023_04_07_054840_create_carts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cid')->comment("Müşteri id si");
+            $table->integer('pid')->comment("Ürün id si");
+            $table->string('size')->nullable()->comment("ürünün bedeni");
+            $table->integer('number')->default(0)->comment("Ürün adedi");
+            $table->double('tprice')->default(0)->comment("ürün toplam adedi");
+            $table->timestamps();
+            $table->foreign('cid')
+            ->references('id')
+            ->on('users')->onDelete('cascade');
+            $table->foreign('pid')
+            ->references('id')
+            ->on('products')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
